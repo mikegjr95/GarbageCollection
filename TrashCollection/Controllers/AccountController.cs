c0249add958650f080181fd5dd02b7d8ce9d8f00
Michael Gonzalez Jr <mikegjr95@gmail.com>
2020-01-07 09:37:13 -0600

    finished my security and roles base implementations

--- TrashCollection/Controllers/AccountController.cs
+++ TrashCollection/Controllers/AccountController.cs
@@ -77,7 +77,7 @@ namespace TrashCollection.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -169,6 +169,7 @@ namespace TrashCollection.Controllers
 
                     return RedirectToAction("Index", "Home");
                 }
+                ViewBag.Name = new SelectList(context.Roles.Where(u => !u.Name.Contains("Admin")).ToList(), "Name", "Name");
                 AddErrors(result);
             }
 
--- TrashCollection/Models/AccountViewModels.cs
+++ TrashCollection/Models/AccountViewModels.cs
@@ -49,9 +49,9 @@ namespace TrashCollection.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
+        [Display(Name = "UserName")]
         [EmailAddress]
-        public string Email { get; set; }
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
